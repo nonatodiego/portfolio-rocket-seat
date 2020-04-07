dd579f3eea375596fe00f8d56bbfe178c4a941aa
Diego Nonato <nonatodiego@hotmail.com>
2020-04-07 16:55:50 -0300

    final project |  portfolio - Rocketseat

---
+++ data.js
@@ -0,0 +1,46 @@
+module.exports = [
+  {
+    id:"vmhti7bDmCQ",
+    title: "BIRDS OF PREY Trailer (2020)",
+    duration: "30 min",
+    price: "Free",
+    featured: true
+  },
+
+  {
+    id:"ScMjOrrfRNE",
+    title: "BLACK WIDOW Trailer (2020)",
+    duration: "30 min",
+    price: "Free"
+  },
+
+  {
+    id:"yGY484EPe5U",
+    title: "WONDER WOMAN 2 Official",
+    duration: "30 min",
+    price: "Free"
+  },
+
+  {
+    id:"_qyw6LC5pnE",
+    title: "FAST AND FURIOUS 9 Trailer",
+    duration: "30 min",
+    price: "Free",
+    featured: true
+  },
+
+  {
+    id:"9S8JXXVx6sc",
+    title: "ANT MAN 2 Trailer Teaser",
+    duration: "30 min",
+    price: "Free"
+  },
+
+  {
+    id:"9Yam5B_iasY",
+    title: "AQUAMAN Trailer 2",
+    duration: "30 min",
+    price: "Free",
+    featured: true
+  }
+]
